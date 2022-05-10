59407bdb564956390df2d6b8eb6c03383edff548
ZeldoRetro <38571849+ZeldoRetro@users.noreply.github.com>
2022-05-10 17:17:48 -0400

    Add files via upload

--- data/maps/other_dimension/change.lua
+++ data/maps/other_dimension/change.lua
@@ -3,13 +3,6 @@ local game = map:get_game()
 
 function map:on_started()
 
-  sword_level = game:get_ability("sword")
-  game:set_ability("sword",0)
-  item_assigned_slot_1 = game:get_item_assigned(1)
-  game:set_item_assigned(1, nil)
-  item_assigned_slot_2 = game:get_item_assigned(2)
-  game:set_item_assigned(2, nil)
-
   game:set_pause_allowed(false)
   game:set_hud_enabled(false)
   hero:set_visible(false)
@@ -23,12 +16,6 @@ function map:on_opening_transition_finished()
     m:set_speed(256)
     m:set_smooth(true)
     m:start(hero)
-
-    sol.timer.start(map,2500,function()
-            game:set_ability("sword",sword_level)
-            game:set_item_assigned(1, item_assigned_slot_1)
-            game:set_item_assigned(2, item_assigned_slot_2)
-    end)
 end
 
 map:register_event("on_finished", function(map)
